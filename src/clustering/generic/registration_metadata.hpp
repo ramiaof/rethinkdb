7e3cb77fd0caea6df0b1aad580fc92fa45eac0fa
Sam Hughes <sam@rethinkdb.com>
2014-06-13 16:48:19 -0700

    Made registration_metadata.hpp not include typed.hpp.

--- src/clustering/generic/registration_metadata.hpp
+++ src/clustering/generic/registration_metadata.hpp
@@ -1,9 +1,9 @@
-// Copyright 2010-2012 RethinkDB, all rights reserved.
+// Copyright 2010-2014 RethinkDB, all rights reserved.
 #ifndef CLUSTERING_GENERIC_REGISTRATION_METADATA_HPP_
 #define CLUSTERING_GENERIC_REGISTRATION_METADATA_HPP_
 
 #include "containers/uuid.hpp"
-#include "rpc/mailbox/typed.hpp"
+#include "rpc/mailbox/addr.hpp"
 #include "rpc/semilattice/joins/macros.hpp"
 
 template<class business_card_t>
@@ -12,19 +12,21 @@ class registrar_business_card_t {
 public:
     typedef uuid_u registration_id_t;
 
-    typedef mailbox_t<void(registration_id_t, peer_id_t, business_card_t)> create_mailbox_t;
-    typename create_mailbox_t::address_t create_mailbox;
 
-    typedef mailbox_t<void(registration_id_t)> delete_mailbox_t;
-    typename delete_mailbox_t::address_t delete_mailbox;
+    typedef mailbox_addr_t<void(registration_id_t, peer_id_t, business_card_t)>
+        create_mailbox_addr_t;
+    typedef typename create_mailbox_addr_t::mailbox_type create_mailbox_t;
+    create_mailbox_addr_t create_mailbox;
+
+    typedef mailbox_addr_t<void(registration_id_t)> delete_mailbox_addr_t;
+    typedef typename delete_mailbox_addr_t::mailbox_type delete_mailbox_t;
+    delete_mailbox_addr_t delete_mailbox;
 
     registrar_business_card_t() { }
 
-    registrar_business_card_t(
-            const typename create_mailbox_t::address_t &cm,
-            const typename delete_mailbox_t::address_t &dm) :
-        create_mailbox(cm), delete_mailbox(dm)
-        { }
+    registrar_business_card_t(const create_mailbox_addr_t &cm,
+                              const delete_mailbox_addr_t &dm)
+        : create_mailbox(cm), delete_mailbox(dm) { }
 
     RDB_MAKE_ME_SERIALIZABLE_2(create_mailbox, delete_mailbox);
 };
